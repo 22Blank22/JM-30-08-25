5e37fd763ef8e71fd0c998c491dcf3b36cf9f3c1
22Blank22 <mac.bibombel@googlemail.com>
2025-09-01 08:31:06 +0200

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,9 +13,8 @@ export class App {
   protected readonly title = signal('timer');
 
   importantDates = [
-    //new Date(2000, 1, 2, 5, 59), // birthday 02.02.2000 05:59
-    new Date(2025, 3, 30, 11, 30), // standesamt 30.04.2025 11:30
-    new Date(2025, 7, 30, 14, 0), // kirchlich 30.08.2025 14:00
+    new Date(2025, 3, 30, 11, 30), 
+    new Date(2025, 7, 30, 14, 0), 
   ];
 
   dateOneActive = false;
